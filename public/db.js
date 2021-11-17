bdb92ea5e7f0145b8f2babcb3c4a87ac389cf308
Emily Dorgan <wmdorgan@gmail.com>
2021-11-16 19:23:49 -0800

    set up checkDatabase() so index db items are added to the database when event listener is triggered and the indexdb gets cleared

--- public/db.js
+++ public/db.js
@@ -32,7 +32,32 @@ function saveRecord(record){
 }
 
 function checkDatabase(){
-    //handles reconnection to database
+    //create a transaction to our indexDB
+    const transaction = db.transaction(["pending"], "readwrite");
+    // access our indexDB
+    const store = transaction.objectStore("pending");
+    // use getAll() method to get all the records
+    const getAll = store.getAll();
+    // Make a POST request to bulk add all transactions in indexDB to the database 
+    getAll.onsuccess = function() {
+        if(getAll.result.length > 0){
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                //if successful, open a transaction to the indexDB, get the object store, and clear all items
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+                store.clear();
+            });
+        }
+    };
 }
 
 // event listener for when the app comes back online
