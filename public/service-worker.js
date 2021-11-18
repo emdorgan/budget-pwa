723861bb00fe34f19cd2d24462c2ef4472617f51
Emily Dorgan <wmdorgan@gmail.com>
2021-11-17 17:10:53 -0800

    set up the service-worker.js, set up the file names to cache in an array, added service-worker.js script to index.html

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+const FILES_TO_CACHE = [
+    "/",
+    "index.html",
+    "styles.css",
+    "db.js",
+    "index.js",
+];
+
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
