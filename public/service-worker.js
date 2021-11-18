50a4f2ec85170ef7a03bd8992a7eaa578424135a
Emily Dorgan <wmdorgan@gmail.com>
2021-11-18 09:37:15 -0800

    updated the FILES_TO_CACHE and cleaned up file structure of service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,21 @@
 const FILES_TO_CACHE = [
     "/",
-    "index.html",
-    "styles.css",
-    "db.js",
-    "index.js",
+    "/styles.css",
+    "/db.js",
+    "/index.js",
 ];
 
 const STATIC_CACHE = "static-cache-v1";
 const RUNTIME_CACHE = "runtime-cache";
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-      caches
-        .open(STATIC_CACHE)
-        .then(cache => cache.addAll(FILES_TO_CACHE))
-        .then(() => self.skipWaiting())
-    );
-  });
+  event.waitUntil(
+    caches
+      .open(STATIC_CACHE)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
 
   // The activate handler takes care of cleaning up old caches.
 self.addEventListener("activate", event => {
@@ -39,49 +38,49 @@ self.addEventListener("activate", event => {
         })
         .then(() => self.clients.claim())
     );
-  });
+});
 
-  self.addEventListener("fetch", event => {
-    // non GET requests are not cached and requests to other origins are not cached
-    if (
-      event.request.method !== "GET" ||
-      !event.request.url.startsWith(self.location.origin)
-    ) {
-      event.respondWith(fetch(event.request));
-      return;
-    }
-  
-    // handle runtime GET requests for data from /api routes
-    if (event.request.url.includes("/api/transaction")) {
-      // make network request and fallback to cache if network request fails (offline)
-      event.respondWith(
-        caches.open(RUNTIME_CACHE).then(cache => {
-          return fetch(event.request)
-            .then(response => {
-              cache.put(event.request, response.clone());
-              return response;
-            })
-            .catch(() => caches.match(event.request));
-        })
-      );
-      return;
-    }
-  
-    // use cache first for all other requests for performance
+self.addEventListener("fetch", event => {
+  // non GET requests are not cached and requests to other origins are not cached
+  if (
+    event.request.method !== "GET" ||
+    !event.request.url.startsWith(self.location.origin)
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  // handle runtime GET requests for data from /api routes
+  if (event.request.url.includes("/api/transaction")) {
+    // make network request and fallback to cache if network request fails (offline)
     event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-  
-        // request is not in cache. make network request and cache the response
-        return caches.open(RUNTIME_CACHE).then(cache => {
-          return fetch(event.request).then(response => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
+      caches.open(RUNTIME_CACHE).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => caches.match(event.request));
       })
     );
-  });
+    return;
+  }
+
+  // use cache first for all other requests for performance
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+        // request is not in cache. make network request and cache the response
+      return caches.open(RUNTIME_CACHE).then(cache => {
+        return fetch(event.request).then(response => {
+          return cache.put(event.request, response.clone()).then(() => {
+            return response;
+          });
+        });
+      });
+    })
+  );
+});
